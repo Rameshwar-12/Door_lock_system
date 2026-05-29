94969252f8989d8239a018c2aef06531b28a42f2
Rameshwar-12 <rameshwarb1218@gmail.com>
2026-05-29 16:28:59 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,39 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_rameshwar_door_lock (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+
+    input  wire ena,
+    input  wire clk,
+    input  wire rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+wire match;
+wire unlock;
+wire error;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+// Password Checker
+assign match = (ui_in[3:0] == 4'b1010);
+
+// Lock Controller
+assign unlock = match;
+assign error  = ~match;
+
+// Outputs
+assign uo_out[0] = unlock;
+assign uo_out[1] = error;
+assign uo_out[7:2] = 6'b000000;
+
+// Unused bidirectional pins
+assign uio_out = 8'b00000000;
+assign uio_oe  = 8'b00000000;
+
+// Prevent warnings
+wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 
 endmodule
